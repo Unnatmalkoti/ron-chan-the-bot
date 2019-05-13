f58abd3ae81243f94b0b411d5d46c10a08c5aad9
Unnat Malkoti <unnatmalkoti@gmail.com>
2019-05-13 13:56:33 +0530

    help

--- commands/help.js
+++ commands/help.js
@@ -7,8 +7,8 @@ module.exports = {
         +"`delete_notif` : To delete a notification\r\n"
         +"`help` : do i need to explain this?\r\n"
         +"`list` : Lists all the notification\r\n"
-        +"`refresh_time` : To Change the time interval after which bot looks for new chapter   *I didn't make it yet*"
-        +"`i_like` : To add notification role for your fav series"
+        +"`refresh_time` : To Change the time interval after which bot looks for new chapter   *I didn't make it yet*\r\n"
+        +"`i_like` : To add notification role for your fav series\r\n"
         +"`i_no_like` : To remove notification role of a series you are not good enough for"
         message.channel.send(helpText);		
 	},
