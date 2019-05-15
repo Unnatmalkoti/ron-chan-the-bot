e98c5d8f4e5d0c4908d4fb91ef694a6f17c82c69
Unnat Malkoti <unnatmalkoti@gmail.com>
2019-05-15 11:47:59 +0530

    Added Diagnostic tools

---
+++ commands/configfiledownload.js
@@ -0,0 +1,32 @@
+const Discord = require('discord.js');
+const fs = require('fs');
+
+module.exports = {
+        name: 'config',
+        aliases: ["config_file","configfile"],
+
+	execute(message, args, data) {
+        if(!message.member.hasPermission("ADMINISTRATOR"))
+        {
+            return;
+        }
+        if(args.length === 0)
+        {
+            const buffer = fs.readFileSync("./config.json");
+            message.channel.sendFile(buffer, "config.json");
+        }
+        if(args[0] === "restart")
+        {
+            message.channel.sen("Restarting my lord...");
+            process.exit();
+        }
+
+        if(args[0] === "logs")
+        {
+            const buffer = fs.readFileSync("./log.txt");
+            message.channel.sendFile(buffer, "config.txt");
+        }
+        
+        
+	},
+};
--- index.js
+++ index.js
@@ -1,12 +1,15 @@
 const Discord = require('discord.js');
 const request = require('request');
-const { prefixes, token } = require('./config.json');
+const { prefixes } = require('./config.json');
 const fs = require('fs');
 const client = new Discord.Client();
 
 
+
 client.commands = new Discord.Collection();
 const commandFiles = fs.readdirSync('./commands').filter(file => file.endsWith('.js'));
+
+//datastreams
 
 
 
@@ -15,49 +18,62 @@ for (const file of commandFiles) {
 	client.commands.set(command.name, command);
 }
 readData();
+
 let i = 10; 
-console.log(i);
 
 
 function readData() {
     fs.readFile("./config.json", 'utf8', (err, jsonString) => {
         console.log("Reading file");
+        log("Reading file..");
         if (err) {
             console.log("Failed to read", err)
-            return
+            log("Failed to read" + err.toString());
+            return;
         }
         try {
             data = JSON.parse(jsonString);
+            data.release_channel_id = process.env.CHANNEL_ID;
         }
         catch (err) {
 
         }
         //console.log(data);
+
     });
+
 }
 
 function updateDatabase()
 {
     const jsonString = JSON.stringify(data)
-    fs.writeFile('./config.json', jsonString, err => {
-        if (err) {
-            console.log('Error writing file', err)
-        } else {
-            console.log('Successfully wrote file')
-        }
-    })
+    // fs.writeFile('./config.json', jsonString, err => {
+    //     if (err) {
+    //         console.log('Error writing file', err)
+    //         log('Error writing file' + err.toString())
+    //     } else {
+    //         console.log('Successfully wrote file')
+    //         log('Successfully wrote file');
+    //     }
+    // })
+
+    var configStream = fs.createWriteStream("./config.json");
+    configStream.write(jsonString);
+    configStream.end();
+
 }
 
 client.once('ready', () => {
     console.log("ready"); 
     //console.log(data);
+    console.log(data.release_channel_id);
     setInterval(function(){getAllChapters(); console.log("Checked!")}, data.refresh_time);    
 })
 
-client.login(token);
+client.login(process.env.DISCORD_TOKEN);
 
 client.on('message', message => {
-    console.log(`\n${message.author.username} : \n${message.content}\n`);
+    //console.log(`\n${message.author.username} : \n${message.content}\n`);
 
     if (message.author.bot) return;
     i = randomWack(i, message, data.random_wack_width);              //randomWack setup
@@ -94,18 +110,20 @@ client.on('message', message => {
 function getAllChapters() {
     try
     {
+        //log("Getting all Chapters...");
         data.series.forEach(manga => {
             let currentManga;
+            
             request({ url: `https://mangadex.org/api/manga/${manga.manga_id}/`, json: true }, function (err, res, json) {
-            //console.log(json);
-    
+            //console.log(json);        
             findNewChapters(json, manga.manga_id, manga.role_id);
             });      
         });
+
     }
     catch(err)
     {
-
+        log("ERROR : "+ err.toString());
     }
 
 
@@ -123,6 +141,7 @@ function findNewChapters(manga_json, manga_id, role_id)
              {
                 if (!data.notified_chapters.includes(chapter_id) && chapter.lang_code === 'gb')
                 {
+                    log("NEW CHAPTER:" + manga_json.manga.title + ` Chapter ${chapter.chapter} ID ${chapter_id} `);  
                     sendNotification(chapter, manga_json.manga, chapter_id, manga_id, role_id);
              
                 }
@@ -132,6 +151,7 @@ function findNewChapters(manga_json, manga_id, role_id)
     catch(err)
     {
         console.log(err);
+        log("ERROR : "+ err.toString());
     }
 
    
@@ -152,12 +172,27 @@ function sendNotification(newChapter, manga, chapter_id, manga_id, role_id)
         const release_channel = client.channels.get(data.release_channel_id)
         const all_role = release_channel.guild.roles.get(data.all_role);
         console.log(release_channel);
-        if(release_channel.send(`${release_channel.guild.roles.get(role_id)} ${all_role}`))
-        if(release_channel.send(emmbed))
-        {
-            data.notified_chapters.push(chapter_id);
-            updateDatabase();
-        }       
+        release_channel.send(`${release_channel.guild.roles.get(role_id)} ${all_role}`)
+        .then(message=>{
+
+            log(`Chapter Id : ${chapter_id} Pinged`);
+            release_channel.send(emmbed)
+                .then(message => {
+                    data.notified_chapters.push(chapter_id);
+                    log(`Chapter Id : ${chapter_id} Embed sent`);
+                    updateDatabase();
+                })
+            .catch(err=>
+                {
+                    log("ERROR : " + err.toString());
+                });
+
+        })            
+        .catch(err=>
+            {
+                log("ERROR : " + err.toString());
+            });;
+        
            
     }
 
@@ -168,20 +203,52 @@ function sendNotification(newChapter, manga, chapter_id, manga_id, role_id)
 
 }   
 
+function log(logString)
+{
+    var logStream = fs.createWriteStream("log.txt", {flags:'a'});
+    logStream.write(getDateTime() + " :: :: " +logString + "\r\n\r\n");
+    logStream.end();
+
+}
+
 function randomWack(messageThreshold, message, width)
 {
     try
     {
         if (messageThreshold <= 0) {
             
-            const min = 5000;
+            const min = 1000;
             messageThreshold = Math.floor((Math.random()*width) + min);
             message.channel.send("Wack!");
         }
-        console.log(`RandomWack: ${messageThreshold}`);
     }
     catch
     {}
     
     return messageThreshold-1;
+}
+
+function getDateTime() {
+
+    var date = new Date();
+
+    var hour = date.getHours();
+    hour = (hour < 10 ? "0" : "") + hour;
+
+    var min  = date.getMinutes();
+    min = (min < 10 ? "0" : "") + min;
+
+    var sec  = date.getSeconds();
+    sec = (sec < 10 ? "0" : "") + sec;
+
+    var year = date.getFullYear();
+
+    var month = date.getMonth() + 1;
+    month = (month < 10 ? "0" : "") + month;
+
+    var day  = date.getDate();
+    day = (day < 10 ? "0" : "") + day;
+
+    return year + ":" + month + ":" + day + ":" + hour + ":" + min + ":" + sec;
+
 }
